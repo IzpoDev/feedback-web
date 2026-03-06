0eab4f29f4f9836d0bc757547f765aff6c5057c9
izpoDev17 <ponce.1714.frp@gmail.com>
2026-03-06 15:42:22 -0500

    dev:Version local para pruebas simples y nuevos cambios

--- src/app/env/env.ts
+++ src/app/env/env.ts
@@ -1,5 +1,5 @@
 export const env = {
-  production: true,
-//  url : 'http://localhost:8082' -> Para preubas locales
-  url: 'https://feedback-anonymous-excercise-1.onrender.com'
+  production: false,
+   url : 'http://localhost:8082' //-> Para preubas locales
+  //url: 'https://feedback-anonymous-excercise-1.onrender.com'
 }
