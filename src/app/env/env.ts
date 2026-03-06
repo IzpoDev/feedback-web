ff038cf60aa430d83a68e247eb44a6291d319aae
izpoDev17 <ponce.1714.frp@gmail.com>
2026-03-06 15:16:45 -0500

    deploy:configuraciones para despliegue con firebase

--- src/app/env/env.ts
+++ src/app/env/env.ts
@@ -1,4 +1,5 @@
 export const env = {
-  production: false,
-  url : 'http://localhost:8082'
+  production: true,
+//  url : 'http://localhost:8082' -> Para preubas locales
+  url: 'https://feedback-anonymous-excercise-1.onrender.com'
 }
