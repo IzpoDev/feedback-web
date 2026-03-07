65f5fd4d83cbcf546cb319002d315c3442380fea
IzpoDev <ponce.1714.frp@gmail.com>
2026-03-07 07:19:20 -0500

    fix:Solucion del problema de renderizado par la pagina de feedbnack

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'feedback/:id',
+    renderMode: RenderMode.Server // También puedes usar RenderMode.Client si no necesitas SEO en esa vista
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
